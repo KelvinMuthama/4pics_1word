aa1bd013584d37437a7c2c46bbf4dd4bb1843dc9
KelvinMuthama <kelvinkamau387@gmail.com>
2019-05-26 21:32:48 +0300

    Decided to use functional programming

---
+++ solver.py
@@ -0,0 +1,9 @@
+"""This module holds the code for pattern matching the user's input  to give the probability of the word. Basically what happens is this, a uuser inputs the letters in the game and the length of the word, then the program gives the user all the possible words that can be formed by the letters given without repeating any letter. The words given are of the specified length given."""
+
+import re
+import sys
+
+from words import words
+
+
+
